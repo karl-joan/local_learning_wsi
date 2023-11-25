8bfe3be3c6eed7062e1a89559eb7161a6c5ce682
Karl-Joan Alesma <karl.joan.alesma@gmail.com>
2023-11-25 10:11:06 +0200

    Fix hook names

--- trainer.py
+++ trainer.py
@@ -144,7 +144,7 @@ class LocalModule(pl.LightningModule):
 
         return {"loss": loss, "y_prob_batch": y_prob.detach(), "label_batch": label.detach()}
 
-    def training_epoch_end(self, outs):
+    def on_training_epoch_end(self, outs):
         y_prob = torch.cat([o["y_prob_batch"] for o in outs if o is not None and o["y_prob_batch"] is not None], dim=0)
         label = torch.cat([o["label_batch"] for o in outs if o is not None and o["label_batch"] is not None], dim=0)
         metrics = self.train_metrics(y_prob, label)
@@ -154,7 +154,7 @@ class LocalModule(pl.LightningModule):
         sch = self.lr_schedulers()
         sch.step()
 
-    def validation_epoch_end(self, outs_dl):
+    def on_validation_epoch_end(self, outs_dl):
         for outs, metrics_fn in zip(outs_dl, [self.eval_metrics, self.eval_test_metrics]):
             y_prob = torch.cat([o["y_prob_batch"] for o in outs if o is not None], dim=0)
             label = torch.cat([o["label_batch"] for o in outs if o is not None], dim=0)
@@ -162,7 +162,7 @@ class LocalModule(pl.LightningModule):
             if not self.trainer.sanity_checking:
                 self.logger.log_metrics(metrics, step=self.current_epoch)
 
-    def test_epoch_end(self, outs):
+    def on_test_epoch_end(self, outs):
         y_prob = torch.cat([o["y_prob_batch"] for o in outs], dim=0)
         label = torch.cat([o["label_batch"] for o in outs], dim=0)
         metrics = self.test_metrics(y_prob, label)
